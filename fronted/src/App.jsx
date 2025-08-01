4ff6fc5c508af197ea70857f3c2e5747eb59b30c
Vinod Tayde <vttayde@gmail.com>
2025-08-01 13:44:29 +0530

    Fixed Importing

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes,Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Collection from './pages/Collection'
 import Navbar from './components/Navbar'
 import About from './pages/About'

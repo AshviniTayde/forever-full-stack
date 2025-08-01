bf7a3562624d19a7076e959f29128817709fb427
Vinod Tayde <vttayde@gmail.com>
2025-08-01 13:42:36 +0530

    Update typing mistake

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,8 @@ import { ToastContainer,toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useEffect } from 'react'
 
- export const backendUrl= import.meta.env.VITE_BAKEND_URL
+ export const backendUrl = import.meta.env.VITE_BACKEND_URL
+
 export const currency='₹'
 const App = () => {
 
